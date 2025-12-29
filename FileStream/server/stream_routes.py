5253c9afe8ffe2f7c073f80b9688aab3deb3b1aa
kaustav9370-lang <kaustav9370@gmail.com>
2025-12-29 15:03:02 +0530

    Update stream_routes.py

--- FileStream/server/stream_routes.py
+++ FileStream/server/stream_routes.py
@@ -7,7 +7,7 @@ from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
 from FileStream.bot import multi_clients, work_loads, FileStream
 from FileStream.config import Telegram, Server
-from FileStream.server.exceptions import FIleNotFound, InvalidHash
+from FileStream.exceptions import FIleNotFound, InvalidHash
 from FileStream import utils, StartTime, __version__
 from FileStream.utils.render_template import render_page
 
@@ -133,4 +133,4 @@ async def media_streamer(request: web.Request, db_id: str):
             "Content-Disposition": f'{disposition}; filename="{file_name}"',
             "Accept-Ranges": "bytes",
         },
-    )
+        )
